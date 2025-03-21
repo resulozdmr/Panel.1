7ad06cf356f325b0ab1c381625cfc4e9ebcf82d3
Resul Özdemir <119076568+resulozdmr@users.noreply.github.com>
2025-03-21 19:12:56 +0300

    yeniden route.ts

--- app/api/posts/[postId]/comments/route.ts
+++ app/api/posts/[postId]/comments/route.ts
@@ -2,7 +2,7 @@ import connectDB from "@/lib/db";
 import { Post } from "@/models/post.model";
 import { NextRequest, NextResponse } from "next/server";
 
-// fetch all comments
+// Tüm yorumları çekmek için GET endpoint
 export async function GET(
   req: NextRequest,
   { params }: { params: { postId: string } }
@@ -11,22 +11,22 @@ export async function GET(
     // Veritabanına bağlan
     await connectDB();
 
-    // İlgili postu bul
+    // Belirtilen postu bul
     const post = await Post.findById(params.postId);
     if (!post) {
       return NextResponse.json({ error: "Post not found." }, { status: 404 });
     }
 
-    // Post bulunduysa yorumları populate et
+    // Post bulunduysa, yorumları populate et (tarihe göre azalan sıralama)
     const comments = await post.populate({
       path: "comments",
       options: { sort: { createdAt: -1 } },
     });
 
-    // Yorumları JSON olarak döndür
+    // Yorumları JSON formatında döndür
     return NextResponse.json(comments);
   } catch (error) {
-    console.error(error);
+    console.error("GET endpoint hatası:", error);
     return NextResponse.json({ error: "An error occurred." }, { status: 500 });
   }
 }
