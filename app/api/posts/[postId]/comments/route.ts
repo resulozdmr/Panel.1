506fadb990866a6888b9ca396f33d1a2687d6054
Resul Özdemir <119076568+resulozdmr@users.noreply.github.com>
2025-03-21 19:07:39 +0300

    Update route.ts

--- app/api/posts/[postId]/comments/route.ts
+++ app/api/posts/[postId]/comments/route.ts
@@ -3,18 +3,30 @@ import { Post } from "@/models/post.model";
 import { NextRequest, NextResponse } from "next/server";
 
 // fetch all comments
-export const GET = async (req:NextRequest, {params}:{params:{postId:string}}) => {
-    try {
-        await connectDB();
-        const post = Post.findById({_id:params.postId});
-        if(!post) return NextResponse.json({error:"Post not found."});
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    // Veritabanına bağlan
+    await connectDB();
 
-        const comments = await post.populate({
-            path:'comments',
-            options:{sort:{createdAt:-1}},
-        });
-        return NextResponse.json(comments);
-    } catch (error) {
-        return NextResponse.json({error:'an error occurred.'});
+    // İlgili postu bul
+    const post = await Post.findById(params.postId);
+    if (!post) {
+      return NextResponse.json({ error: "Post not found." }, { status: 404 });
     }
+
+    // Post bulunduysa yorumları populate et
+    const comments = await post.populate({
+      path: "comments",
+      options: { sort: { createdAt: -1 } },
+    });
+
+    // Yorumları JSON olarak döndür
+    return NextResponse.json(comments);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "An error occurred." }, { status: 500 });
+  }
 }
