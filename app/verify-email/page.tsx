2c9dd83d5b4102c8a0ceeb26e63e3bba97724803
resulozdmr <resulozdmr6@icloud.com>
2025-03-24 07:36:44 +0300

    sasda

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -11,20 +12,20 @@ export default function VerifyEmailPage() {
       router.push("/sign-in");
     }, 5000); // 5 saniye sonra yönlendirme
 
-    return () => clearTimeout(timer);
+    return () => clearTimeout(timer); // Cleanup
   }, [router]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50 px-4">
-      <img src="/logo.png" alt="DrGulf Logo" className="h-20 mb-4" />
+      <Image src="/logo.png" alt="DrGulf Logo" height={80} width={80} />
       <h2 className="text-xl font-semibold mb-2">Please verify your email</h2>
       <p className="text-gray-600 text-center max-w-md">
         We've sent you an email with a verification link. Please check your inbox
         and confirm your email address to activate your account.
       </p>
       <p className="mt-4 text-sm text-gray-400">
-  You&apos;ll be redirected to the login page shortly...
-</p>
+        You&apos;ll be redirected to the login page shortly...
+      </p>
     </div>
   );
 }
